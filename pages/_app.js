bdf64ed589139a5626d544fab8bc51d71f545372
Michael Hutchings <mdhutchings94@gmail.com>
2022-05-29 14:57:45 +0100

    add link to github

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <div className="absolute top-0 right-0 p-4 w-16 h-16">
+        <a href="https://github.com/midanosi/mksc-videos-static">
+          <img src="/GitHub-Mark-64px.png" />
+        </a>
+      </div>
+    </>
+  );
 }
